ce3ffcfc75dd18f1490e914ce17143a6735bb5ca
Tigran Vardanyan <vardanyant.89@gmail.com>
2018-03-06 18:10:11 +0400

    data types

---
+++ PythonStarterFirstSteps/PythonVariablesAndDataTypes/data_types.py
@@ -0,0 +1,13 @@
+
+#------------------------int
+number = 1254856
+print(number)
+string = '158496'
+number = int(string)
+print(number +5)
+#------------------------------bool
+bool1 = True
+bool2 = False
+print(bool1)
+print(bool2)
+#-------------------------------float
